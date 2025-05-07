7e35f1ae25be1751a132800947e816893166b7d7
Daniel Rondon <rondondaniel@gmail.com>
2025-05-07 16:15:51 +0200

    [Feat] Amplify implementation done

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,32 @@
 import { defineBackend } from '@aws-amplify/backend';
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+const bedrockDataSource = backend.data.resources.graphqlApi.addHttpDataSource(
+    "bedrockDS",
+    "https://bedrock-runtime.eu-west-3.amazonaws.com",
+    {
+      authorizationConfig: {
+        signingRegion: "eu-west-3",
+        signingServiceName: "bedrock",
+      },
+    }
+  );
+
+  bedrockDataSource.grantPrincipal.addToPrincipalPolicy(
+    new PolicyStatement({
+      resources: [
+        "arn:aws:bedrock:eu-west-3::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0",
+      ],
+      actions: ["bedrock:InvokeModel"],
+    })
+  );
